311a4a037d3e777f7e2fe089f8ef755b102cf6d3
zqc970816 <1245910840@qq.com>
2020-12-09 16:35:09 +0800

    updated

--- paddleseg/core/val.py
+++ paddleseg/core/val.py
@@ -3,7 +3,7 @@
 Author: TJUZQC
 Date: 2020-11-25 16:12:55
 LastEditors: TJUZQC
-LastEditTime: 2020-12-09 16:31:07
+LastEditTime: 2020-12-09 16:34:51
 Description: None
 '''
 # Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
@@ -48,7 +48,6 @@ def evaluate(model, eval_dataset=None, iter_id=None):
         im = paddle.to_tensor(im)
         logits = model(im)
         pred = paddle.argmax(logits[0], axis=1) if eval_dataset.num_classes > 1 else paddle.nn.functional.sigmoid(logits[0])
-        print(pred.shape, label.shape)
         pred = pred.numpy().astype('float32')
         if eval_dataset.num_classes == 1:
             pred[pred >= 0.5] = 1.
@@ -68,6 +67,7 @@ def evaluate(model, eval_dataset=None, iter_id=None):
         pred = pred.astype('int64')
         mask = label != eval_dataset.ignore_index
         # To-DO Test Execution Time
+        print(pred.shape, label.shape)
         conf_mat.calculate(pred=pred, label=label, mask=mask)
         _, iou = conf_mat.mean_iou()
 
