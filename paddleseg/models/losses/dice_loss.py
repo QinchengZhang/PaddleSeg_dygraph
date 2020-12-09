c138e6cb8c710c97a57ee514f44cb295eb10046d
zqc970816 <1245910840@qq.com>
2020-12-09 15:01:41 +0800

    updated

--- paddleseg/models/losses/dice_loss.py
+++ paddleseg/models/losses/dice_loss.py
@@ -3,7 +3,7 @@
 Author: TJUZQC
 Date: 2020-12-03 14:31:32
 LastEditors: TJUZQC
-LastEditTime: 2020-12-03 14:33:46
+LastEditTime: 2020-12-09 15:00:59
 Description: None
 '''
 import paddle
@@ -29,18 +29,20 @@ class DiceLoss(nn.Layer):
 
         Args:
             input (Tensor): Logit tensor, the data type is float32, float64. Shape is
-                (N, 1), where C is number of classes, and if shape is more than 2D, this
-                is (N, 1, D1, D2,..., Dk), k >= 1.
+                (N, C), where C is number of classes, and if shape is more than 2D, this
+                is (N, C, D1, D2,..., Dk), k >= 1.
             label (Tensor): Label tensor, the data type is int64. Shape is (N), where each
                 value is 0 <= label[i] <= C-1, and if shape is more than 2D, this is
                 (N, D1, D2,..., Dk), k >= 1.
         """
-        assert input.shape[1] == 1, f'The channel of logit except 1 but fot {input.shape[1]}'
-        input = paddle.squeeze(input, 1)
+        label = paddle.unsqueeze(label, 1)
         assert len(label.shape) == len(input.shape), 'The shape of logit and label must be same'
-        if len(label.shape) != len(input.shape):
-            label = paddle.squeeze(label, 1)
+        perm = [i for i in range(len(label.shape))]
+        perm.remove(1)
+        perm.append(1)
 
+        input = paddle.transpose(input, perm)
+        label = paddle.transpose(label, perm)
         out = F.dice_loss(
             input, label, self.EPS)
         label.stop_gradient = True
