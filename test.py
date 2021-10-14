c07787f73a50a79f377b5d5e889a355edc49d61b
zhangqincheng_i <zhangqincheng_i@didiglobal.com>
2021-10-14 20:00:10 +0800

    [update]

--- test.py
+++ test.py
@@ -8,14 +8,14 @@ Description: None
 '''
 import sys
 sys.setrecursionlimit(100000) #例如这里设置为十万
-from paddleseg.models import HS_HRUNet, HRUNet
+from paddleseg.models import HS_HRUNet, HRUNet, HRUNet_MaxPool
 from paddleseg.models.backbones import ResNet50_vd
 import paddle
 import os
 
 if __name__ == '__main__':
     # input = paddle.ones([1, 3, 256, 256])
-    model = HS_HRUNet(2)
+    model = HRUNet_MaxPool(2)
     params = paddle.summary(model, input_size=(1,3,256,256), dtypes='float32')
     # y = model(input)
     # print(y.shape)
